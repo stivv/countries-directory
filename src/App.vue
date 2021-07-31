c9a0b043e05ec0a3f0f8ac703f77b124259619ea
stivv <stevestivv@gmail.com>
2021-07-31 20:31:07 +0300

    rest countries api implementation, updates

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <h4>Countries directory</h4>
+  <div v-for="country in countries" :key="country.name">
+    {{ country.name }}
+  </div>
 </template>
 
 <script setup>
+import { onBeforeMount, ref } from 'vue'
+import axios from 'axios'
+const countries = ref([])
+
+onBeforeMount(() => {
+  axios.get('https://restcountries.eu/rest/v2/all').then(res => {
+    countries.value = res.data
+  })
+})
 </script>
 
